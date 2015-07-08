fe50fe212749ec26e0b05e60c77be2c3fc15385d
Steve Kondik <steve@cyngn.com>
2015-07-08 09:56:01 -0700

    stagefright: Add support for loading a custom OMXPlugin
     * To facilitate moving the stagefright-plugins glue out of the
       framework, support is added to OMXMaster to load multiple
       external plugins besides internal/vendor versions.
     * This is currently limited to one plugin, defined by the
       "mm.sf.omx-plugin" system property. The code will allow any
       number of libraries to be loaded, though.
     * Should also be useful for nonstandard vendor implementations too.
    
    Change-Id: I27d7e16ad56baa17754d8ea47a8c608a0d73d6f1

--- media/libstagefright/omx/OMXStore.cpp
+++ media/libstagefright/omx/OMXStore.cpp
@@ -28,6 +28,8 @@
 #include <dlfcn.h>
 #include <fcntl.h>
 
+#include <cutils/properties.h>
+
 #include <sstream>
 
 namespace android {
@@ -55,6 +57,7 @@ OMXStore::OMXStore() {
 
     addVendorPlugin();
     addPlatformPlugin();
+    addUserPlugin();
 }
 
 OMXStore::~OMXStore() {
@@ -67,6 +70,13 @@ void OMXStore::addVendorPlugin() {
 
 void OMXStore::addPlatformPlugin() {
     addPlugin("libstagefright_softomx_plugin.so");
+}
+
+void OMXStore::addUserPlugin() {
+    char plugin[PROPERTY_VALUE_MAX];
+    if (property_get("media.sf.omx-plugin", plugin, NULL)) {
+        addPlugin(plugin);
+    }
 }
 
 void OMXStore::addPlugin(const char *libname) {
