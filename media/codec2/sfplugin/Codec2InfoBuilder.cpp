8ec67d8c1144d41f69b5b1bef07be58548e011ef
hmtheboy154 <buingoc67@gmail.com>
2025-06-30 06:35:49 +0000

    codec2: Make RGBA/RGBX 8888 into usable formats
    Plane layout code is already in C2AllocatorGralloc so we can just
    use that. What we do now is to map these format to COLOR_FormatRGBAFlexible
    to make it work.
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

--- media/codec2/sfplugin/Codec2InfoBuilder.cpp
+++ media/codec2/sfplugin/Codec2InfoBuilder.cpp
@@ -312,6 +312,8 @@ void addSupportedColorFormats(
         addDefaultColorFormat(COLOR_FormatYUV420SemiPlanar);
         addDefaultColorFormat(COLOR_FormatYUV420PackedPlanar);
         addDefaultColorFormat(COLOR_FormatYUV420PackedSemiPlanar);
+        addDefaultColorFormat(COLOR_Format32bitABGR8888);
+        addDefaultColorFormat(COLOR_FormatRGBAFlexible);
         // Android video codecs prefer CPU-readable formats
         if (trait.name.find("android") != std::string::npos) {
             addDefaultColorFormat(COLOR_FormatSurface);
@@ -756,6 +758,8 @@ status_t Codec2InfoBuilder::buildMediaCodecList(MediaCodecListWriter* writer) {
                     pixelFormatMap[HAL_PIXEL_FORMAT_YCBCR_P010]    = COLOR_FormatYUVP010;
                     pixelFormatMap[HAL_PIXEL_FORMAT_RGBA_1010102]  = COLOR_Format32bitABGR2101010;
                     pixelFormatMap[HAL_PIXEL_FORMAT_RGBA_FP16]     = COLOR_Format64bitABGRFloat;
+                    pixelFormatMap[HAL_PIXEL_FORMAT_RGBA_8888]     = COLOR_Format32bitABGR8888;
+                    pixelFormatMap[HAL_PIXEL_FORMAT_RGBX_8888]     = COLOR_FormatRGBAFlexible;
                     pixelFormatMap[AHARDWAREBUFFER_FORMAT_YCbCr_P210]    = COLOR_FormatYUVP210;
 
                     std::shared_ptr<C2StoreFlexiblePixelFormatDescriptorsInfo> pixelFormatInfo;
