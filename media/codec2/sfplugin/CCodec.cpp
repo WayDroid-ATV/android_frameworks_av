ef7198b96c05dd9690e729adbc0d09c971766bf2
hmtheboy154 <buingoc67@gmail.com>
2025-06-30 06:35:49 +0000

    codec2: Make RGBA/RGBX 8888 into usable formats
    Plane layout code is already in C2AllocatorGralloc so we can just
    use that. What we do now is to map these format to COLOR_FormatRGBAFlexible
    to make it work.
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- media/codec2/sfplugin/CCodec.cpp
+++ media/codec2/sfplugin/CCodec.cpp
@@ -1139,6 +1139,12 @@ void CCodec::configure(const sp<AMessage> &msg) {
                                 format = flexPixelFormat[10];
                             }
                             break;
+                        case COLOR_FormatRGBAFlexible:
+                            format = COLOR_FormatRGBAFlexible;
+                            if (flexPixelFormat.count(8) != 0) {
+                                format = flexPixelFormat[8];
+                            }
+                            break;
                         default:
                             // No-op
                             break;
